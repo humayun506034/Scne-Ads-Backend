ecc3eb9f1b093064008a33166d00cbcc6178a3c9
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2026-02-18 16:12:13 +0600

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5175",
       "http://localhost:5176",
-      "https://scne-ads.vercel.app",
+      "https://scne-ads.netlify.app",
       "*",
     ],
     credentials: true,
